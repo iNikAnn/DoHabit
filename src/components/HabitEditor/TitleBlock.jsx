1710781c1c92584189f4539d9edc53cd131e3a89
Nikolai A. <ilowen@ya.ru>
2024-05-15 15:17:09 +0800

    Update: Added informational messages for incorrect title input when creating a habit

--- src/components/HabitEditor/TitleBlock.jsx
+++ src/components/HabitEditor/TitleBlock.jsx
@@ -1,10 +1,35 @@
 import styles from '../../css/TitleBlock.module.css';
 
+// react
+import { useEffect, useState } from 'react';
+
 function TitleBlock({ input, alreadyExist, onChange }) {
+	// error message
+	const [errorMessage, setErrorMessage] = useState('');
+
+	useEffect(() => {
+		if (alreadyExist) {
+			setErrorMessage(input
+				? 'A habit with this name already exists.'
+				: 'Title field cannot be empty.'
+			);
+		} else {
+			setErrorMessage('');
+		};
+
+		return () => setErrorMessage('');
+	}, [input, alreadyExist]);
+
 	return (
 		<label className={styles.label}>
 			<div className={styles.header}>
 				<h3>Title</h3>
+
+				{alreadyExist && (
+					<small className={styles.errorMessage}>
+						{errorMessage}
+					</small>
+				)}
 			</div>
 
 			<input type="text" name="title" id="title"
