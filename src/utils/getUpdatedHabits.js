e59bde9c7130fe59804bb7a79b0ed469b566888a
Nikolai A. <ilowen@ya.ru>
2024-06-03 22:01:50 +0800

    Refactor: Simplify and improve code

--- src/utils/getUpdatedHabits.js
+++ src/utils/getUpdatedHabits.js
@@ -4,37 +4,41 @@ import editHabit from './editHabit';
 import reorderHabit from './reorderHabit';
 
 function getUpdatedHabits(habits, { data, mode, habitTitle }) {
-	if (mode === 'delete') {
-		return deleteHabit(habits, habitTitle);
-	};
+	let updatedHabits = [...habits];
 
-	const newHabit = {
+	const newHabit = data ? {
 		title: data.title.value,
 		colorIndex: Number(data.colorIndex.value),
 		iconTitle: data.iconTitle.value,
 		frequency: Number(data.frequency.value),
 		completedDays: [],
-	};
+	} : null;
 
-	if (mode === 'edit') {
-		const updatedHabits = editHabit(habits, habitTitle, newHabit);
+	switch (mode) {
+		case 'delete':
+			updatedHabits = deleteHabit(updatedHabits, habitTitle);
+			break;
 
-		// reorder habits
-		if (data.order && data.order.value) {
+		case 'edit':
+			updatedHabits = editHabit(updatedHabits, habitTitle, newHabit);
+
+			// reorder habits
 			const newIndex = Number(data.order.value, 10) - 1;
-			const currIndex = updatedHabits.findIndex((habit) => {
-				return habit.title === data.title.value;
-			});
+			const currIndex = updatedHabits.findIndex((habit) => (
+				habit.title === data.title.value
+			));
 
 			if (currIndex !== -1 && newIndex !== currIndex) {
-				reorderHabit(updatedHabits, newIndex, currIndex);
+				updatedHabits = reorderHabit(updatedHabits, newIndex, currIndex);
 			};
-		};
+			break;
 
-		return updatedHabits;
+		default:
+			updatedHabits = [newHabit, ...updatedHabits];
+			break;
 	};
 
-	return [newHabit, ...habits];
+	return updatedHabits;
 }
 
 export default getUpdatedHabits;
--- src/utils/reorderHabit.js
+++ src/utils/reorderHabit.js
@@ -1,10 +1,12 @@
-function reorderHabit(habits, newIndex, currIndex) {
+function reorderHabit([...habits], newIndex, currIndex) {
 	const currHabit = habits[currIndex];
 
 	if (currIndex !== newIndex && newIndex >= 0 && newIndex < habits.length) {
 		habits.splice(currIndex, 1);
 		habits.splice(newIndex, 0, currHabit);
 	};
+
+	return habits;
 }
 
 export default reorderHabit;
