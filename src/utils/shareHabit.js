15cf4b8c6c54c0f9e6de6f7eb16baa009a137a06
Nikolai A. <ilowen@ya.ru>
2024-06-25 15:52:42 +0800

    Refactor: Minor code adjustments made

--- src/utils/shareHabit.js
+++ src/utils/shareHabit.js
@@ -1,31 +1,31 @@
 import html2canvas from 'html2canvas';
 
 async function shareHabit(dataID) {
-	const habitElement = document.querySelector(`div[data-id='${dataID}']`);
-
-	const options = {
-		backgroundColor: 'black',
-		x: -20,
-		y: -20,
-		width: habitElement.offsetWidth + 40,
-		height: habitElement.offsetHeight + 40,
-		ignoreElements: (el) => el.getAttribute('data-name') === 'habitMenu'
-	};
-
-	const canvas = await html2canvas(habitElement, options);
-	const image = canvas.toDataURL("image/jpeg", 1);
-	const fileName = 'DoHabit_' + new Date().toDateString().replace(/ /g, '_');
-
-	const blob = await fetch(image).then((res) => res.blob());
-	const file = new File([blob], `${fileName}.jpg`, { type: 'image/jpeg' });
-
-	const sharedData = {
-		title: 'Share Habit',
-		text: 'Check out my habit!',
-		files: [file]
-	};
-
 	if ('share' in navigator) {
+		const habitElement = document.querySelector(`div[data-id='${dataID}']`);
+
+		const options = {
+			backgroundColor: 'black',
+			x: -20,
+			y: -20,
+			width: habitElement.offsetWidth + 40,
+			height: habitElement.offsetHeight + 40,
+			ignoreElements: (el) => el.getAttribute('data-name') === 'habitMenu'
+		};
+
+		const canvas = await html2canvas(habitElement, options);
+		const image = canvas.toDataURL("image/jpeg", 1);
+		const fileName = 'DoHabit_' + new Date().toDateString().replace(/ /g, '_');
+
+		const blob = await fetch(image).then((res) => res.blob());
+		const file = new File([blob], `${fileName}.jpg`, { type: 'image/jpeg' });
+
+		const sharedData = {
+			title: 'Share Habit',
+			text: 'Check out my habit!',
+			files: [file]
+		};
+
 		try {
 			await window.navigator.share(sharedData);
 		} catch (error) {
