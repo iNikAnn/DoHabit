a6a94e13b09dfcc9f1f7197c66596c45fe2687e7
Nikolai A. <ilowen@ya.ru>
2024-09-02 20:01:06 +0800

    Update: Update title prop of Placeholder component used in Diary

--- src/components/Diary/Diary.jsx
+++ src/components/Diary/Diary.jsx
@@ -73,7 +73,7 @@ function Diary(props) {
 			) : (
 				<Placeholder
 					image={<InfoSvg />}
-					title="This habit's diary is empty"
+					title={(habitTitle ? "This habit's" : 'Main') + ' diary is empty'}
 					desc="Add your first note to start tracking your progress and thoughts."
 					textOnButton="Add First Note"
 					buttonIcon={<MdStickyNote2 />}
