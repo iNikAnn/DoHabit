2ecd53b3dc09cc30a82da5e00b412da954e8dc35
Nikolai A. <ilowen@ya.ru>
2024-09-07 19:05:45 +0800

    Update: Added the ability to expand the shortened note

--- src/components/Diary/Note.jsx
+++ src/components/Diary/Note.jsx
@@ -1,5 +1,8 @@
 import styles from '../../css/Note.module.css';
 
+// react
+import { useState } from 'react';
+
 function Note({ text, date, onDeleteNote }) {
 	const dateStr = new Date(date).toLocaleDateString();
 	const timeStr = new Date(date).toLocaleTimeString(
@@ -7,9 +10,12 @@ function Note({ text, date, onDeleteNote }) {
 		{ hour: '2-digit', minute: '2-digit' }
 	);
 
-	const maxTextLength = 90;
-	const shortText = text.length > maxTextLength
-		? text.slice(0, 90).trim() + '...'
+	const [isCollapsed, setIsCollapsed] = useState(true);
+
+	const MAX_TEXT_LENGTH = 100;
+	const isTextExceeded = text.length > MAX_TEXT_LENGTH;
+	const displayedText = isTextExceeded && isCollapsed
+		? text.slice(0, MAX_TEXT_LENGTH - 10).trim() + '...'
 		: text;
 
 	return (
@@ -17,7 +23,19 @@ function Note({ text, date, onDeleteNote }) {
 			className={styles.note}
 			onDoubleClick={() => onDeleteNote(date)}
 		>
-			<div>{shortText}</div>
+			<div>
+				{displayedText}
+
+				{(isTextExceeded && isCollapsed) && (
+					<button
+						style={{ marginLeft: '0.6rem' }}
+						className="text-button"
+						onClick={() => setIsCollapsed((prev) => !prev)}
+					>
+						Expand
+					</button>
+				)}
+			</div>
 
 			<span className={styles.desc}>
 				<div className={styles.date}>
