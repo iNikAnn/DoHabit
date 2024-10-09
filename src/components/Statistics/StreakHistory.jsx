8816997ef001d2d3a2564ee9cc1f3ff2882ca1e5
Nikolai A. <ilowen@ya.ru>
2024-10-09 17:39:54 +0800

    Refactor: Minor code adjustments made

--- src/components/Statistics/StreakHistory.jsx
+++ src/components/Statistics/StreakHistory.jsx
@@ -9,39 +9,41 @@ function StreakHistory({ streaks, colorPalette }) {
 	const [listLength, setListLength] = useState(5);
 
 	return (
-		<div className={styles.streaks}>
-			{streaks.slice(0, listLength).map(
-				(s) => (
-					<div
-						style={{ borderColor: darkenedColor }}
-						className={styles.card}
-					>
-						<div className={styles.length}>
-							{s.length}
-						</div>
-
-						<div className={styles.desc}>
-							<div className={styles.dateWrapper}>
-								<small className={`${styles.label} ${styles.start}`}>
-									Start:
-								</small>
-								<small>{s.start}</small>
+		<div className={styles.history}>
+			<ul className={styles.streaks}>
+				{streaks.slice(0, listLength).map(
+					(s, i) => (
+						<li
+							style={{ borderColor: darkenedColor }}
+							className={`${styles.card} ${i === streaks.length - 1 ? styles.isLast : ''}`}
+						>
+							<div className={styles.length}>
+								{s.length}
 							</div>
 
-							<div className={styles.dateWrapper}>
-								<small className={`${styles.label} ${styles.end}`}>
-									End:
-								</small>
-								<small>{s.end}</small>
+							<div className={styles.desc}>
+								<div className={styles.dateWrapper}>
+									<small className={`${styles.label} ${styles.start}`}>
+										Start:
+									</small>
+									<small>{s.start}</small>
+								</div>
+
+								<div className={styles.dateWrapper}>
+									<small className={`${styles.label} ${styles.end}`}>
+										End:
+									</small>
+									<small>{s.end}</small>
+								</div>
 							</div>
-						</div>
-					</div>
-				)
-			)}
+						</li>
+					)
+				)}
+			</ul>
 
 			{listLength < streaks.length && (
 				<button
-					className='text-button'
+					className={`text-button ${styles.showMoreBtn}`}
 					onClick={() => setListLength((curr) => curr + 5)}
 				>
 					Show more
