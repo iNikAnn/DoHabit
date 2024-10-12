91a4cfd66aec4f1c2ebcff9b799acf32f1f8eb7e
Nikolai A. <ilowen@ya.ru>
2024-10-12 15:42:52 +0800

    Refactor: Remove usage of color-mix function in getColorPalette due to html2canvas limitations

--- src/utils/getColorPalette.js
+++ src/utils/getColorPalette.js
@@ -1,8 +1,10 @@
 function getColorPalette(hsl) {
 	return {
 		baseColor: String(hsl),
-		darkenedColor: `color-mix(in hsl, ${hsl}, black 65%)`,
-		softenedColor: `color-mix(in hsl, ${hsl}, dimgray 80%)`
+		// darkenedColor: `color-mix(in hsl, ${hsl}, black 65%)`,
+		darkenedColor: hsl.replace(/(\d{2,3}%), (\d{2,3}%\))/g, '8%, 18%)'),
+		// softenedColor: `color-mix(in hsl, ${hsl}, dimgray 80%)`
+		softenedColor: hsl.replace(/(\d{2,3}%), (\d{2,3}%\))/g, '12%, 50%)')
 	};
 }
 
