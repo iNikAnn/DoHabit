f96868b62a421d27b93e359dfd679b97751069ea
Nikolai A. <ilowen@ya.ru>
2024-10-16 13:23:08 +0800

    Refactor: Minor code adjustments made

--- src/context/settingsContext.js
+++ src/context/settingsContext.js
@@ -2,12 +2,13 @@ import { createContext, useReducer } from 'react';
 
 // utils
 import settingsReducer from '../utils/settingsReducer';
+import initSettings from '../utils/initSettings';
 
 export const SettingsContext = createContext(null);
 export const SettingsDispatchContext = createContext(null);
 
 export function SettingsProvider({ children }) {
-	const [settings, settingsDispatch] = useReducer(settingsReducer, {});
+	const [settings, settingsDispatch] = useReducer(settingsReducer, null, initSettings);
 
 	return (
 		<SettingsContext.Provider value={settings}>
--- src/utils/settingsReducer.js
+++ src/utils/settingsReducer.js
@@ -1,5 +1,20 @@
-function settingsReducer(settings, actions) {
+import saveToLocalStorage from '../utils/saveToLocalStorage';
 
+function settingsReducer(settings, actions) {
+	let newSettings = { ...settings };
+
+	switch (actions.type) {
+		case 'calendar':
+			newSettings.calendar = actions.value;
+			break;
+
+		default:
+			break;
+	}
+
+	saveToLocalStorage('settings', newSettings);
+
+	return newSettings;
 }
 
 export default settingsReducer;
