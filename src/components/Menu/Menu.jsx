b42768b75b88a5193c8d2f3e0297f8c988ee2021
Nikolai A. <ilowen@ya.ru>
2024-10-17 16:22:11 +0800

    Refactor: Simplify Menu component

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,6 +2,7 @@ import styles from '../../css/Menu.module.css';
 import packageJson from '../../../package.json';
 
 // components
+import MenuItemList from './MenuItemList';
 import MenuItem from './MenuItem';
 
 // icons
@@ -14,99 +15,91 @@ import { HiArchiveBox } from "react-icons/hi2";
 function Menu({ onOpenModal }) {
 	return (
 		<section className={styles.menu}>
-			<div className={styles.category}>
-				<h4>App</h4>
+			<MenuItemList title="App">
+				<MenuItem
+					icon={<HiArchiveBox />}
+					iconColor="#7b68ee"
+					title="Archive"
+					desc="View or manage archived habits"
+					onClick={() => onOpenModal({
+						type: 'open',
+						modalTitle: 'Archive',
+						modalContent: 'archive'
+					})}
+					arrow
+				/>
 
-				<ul className={styles.list}>
-					<MenuItem
-						icon={<HiArchiveBox />}
-						iconColor="#7b68ee"
-						title="Archive"
-						desc="View or manage archived habits"
-						onClick={() => onOpenModal({
-							type: 'open',
-							modalTitle: 'Archive',
-							modalContent: 'archive'
-						})}
-						arrow
-					/>
+				<MenuItem
+					icon={<FaPaintBrush />}
+					iconColor="#ffa420"
+					title="Appearance"
+					desc="Customize the app's look"
+					onClick={() => onOpenModal({
+						type: 'open',
+						modalTitle: 'Appearance',
+						modalContent: 'appearanceSettings'
+					})}
+					arrow
+				/>
+			</MenuItemList>
 
-					<MenuItem
-						icon={<FaPaintBrush />}
-						iconColor="#ffa420"
-						title="Appearance"
-						desc="Customize the app's look"
-						onClick={() => onOpenModal({
-							type: 'open',
-							modalTitle: 'Appearance',
-							modalContent: 'appearanceSettings'
-						})}
-						arrow
-					/>
-				</ul>
-			</div>
+			<MenuItemList title="General">
+				<MenuItem
+					icon={<BsFillDatabaseFill />}
+					iconColor="#77dd77"
+					title="Export/Import Data"
+					desc="Backup or restore your habits"
+					onClick={() => onOpenModal({
+						type: 'open',
+						modalContent: 'dataTransfer',
+						modalTitle: 'Export/Import Data'
+					})}
+					arrow
+				/>
 
-			<div className={styles.category}>
-				<h4>General</h4>
+				<MenuItem
+					icon={<FaGithub />}
+					iconColor="#7fc7ff"
+					title="GitHub Repository"
+					desc="View or contribute to the project"
+					onClick={() => window.open('https://github.com/iNikAnn/DoHabit', '_blank')}
+					link
+				/>
 
-				<ul className={styles.list}>
-					<MenuItem
-						icon={<BsFillDatabaseFill />}
-						iconColor="#77dd77"
-						title="Export/Import Data"
-						desc="Backup or restore your habits"
-						onClick={() => onOpenModal({
-							type: 'open',
-							modalContent: 'dataTransfer',
-							modalTitle: 'Export/Import Data'
-						})}
-						arrow
-					/>
+				<MenuItem
+					icon={<IoIosMail />}
+					iconColor="#ffb841"
+					title="Send Feedback"
+					desc="Share your thoughts or report an issue"
+					onClick={() => window.location.href = 'mailto:ilowen@ya.ru?subject=Feedback%20on%20DoHabit'}
+					link
+				/>
+			</MenuItemList>
 
-					<MenuItem
-						icon={<FaGithub />}
-						iconColor="#7fc7ff"
-						title="GitHub Repository"
-						desc="View or contribute to the project"
-						onClick={() => window.open('https://github.com/iNikAnn/DoHabit', '_blank')}
-						link
-					/>
+			<MenuItemList
+				title="Danger Zone"
+				titleStyle={{ color: 'IndianRed' }}
+				listStyle={{ border: '1px solid IndianRed' }}
+			>
+				<MenuItem
+					icon={<ImFire style={{ color: 'IndianRed' }} />}
+					title="Clear Data"
+					desc="Delete all application data"
+					onClick={() => {
+						if (window.confirm('Are you sure you want to delete all application data?')) {
+							const input = window.prompt('Enter the phrase "Delete all data".');
 
-					<MenuItem
-						icon={<IoIosMail />}
-						iconColor="#ffb841"
-						title="Send Feedback"
-						desc="Share your thoughts or report an issue"
-						onClick={() => window.location.href = 'mailto:ilowen@ya.ru?subject=Feedback%20on%20DoHabit'}
-						link
-					/>
-				</ul>
-			</div>
-
-			<div className={styles.category}>
-				<h4 style={{ color: 'IndianRed' }}>Danger Zone</h4>
-
-				<ul className={styles.list}>
-					<MenuItem
-						icon={<ImFire style={{ color: 'IndianRed' }} />}
-						title="Clear Data"
-						desc="Delete all application data"
-						onClick={() => {
-							if (window.confirm('Are you sure you want to delete all application data?')) {
-								const input = window.prompt('Enter the phrase "Delete all data".');
-
-								if (input && input.toLowerCase() === 'delete all data') {
-									localStorage.clear();
-									alert('All data has been successfully removed. The application will now reload.');
-									window.location.reload();
-								} else {
-									alert('You entered the wrong phrase, operation canceled.');
-								};
+							if (input && input.toLowerCase() === 'delete all data') {
+								localStorage.clear();
+								alert('All data has been successfully removed. The application will now reload.');
+								window.location.reload();
+							} else {
+								alert('You entered the wrong phrase, operation canceled.');
 							};
-						}}
-					/>
-				</ul>
-			</div>
+						};
+					}}
+				/>
+			</MenuItemList>
 
 			<div className={`${styles.category} ${styles.footer}`}>
 				<small>Version: {packageJson.version}</small>
