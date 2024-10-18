42353aebdaff855794d1056bc2c53b5130e67e38
Nikolai A. <ilowen@ya.ru>
2024-10-18 13:05:54 +0800

    Refactor: Updated getColorPalette function to return colors for light theme as well

--- src/utils/getColorPalette.js
+++ src/utils/getColorPalette.js
@@ -2,7 +2,7 @@ function getColorPalette(hsl) {
 	return {
 		baseColor: String(hsl),
 		// darkenedColor: `color-mix(in hsl, ${hsl}, black 65%)`,
-		darkenedColor: hsl.replace(/(\d{2,3}%), (\d{2,3}%\))/g, '8%, 18%)'),
+		darkenedColor: `light-dark(${hsl.replace(/(\d{2,3}%), (\d{2,3}%\))/g, '8%, 95%)')}, ${hsl.replace(/(\d{2,3}%), (\d{2,3}%\))/g, '8%, 18%)')})`,
 		// softenedColor: `color-mix(in hsl, ${hsl}, dimgray 80%)`
 		softenedColor: hsl.replace(/(\d{2,3}%), (\d{2,3}%\))/g, '12%, 50%)')
 	};
