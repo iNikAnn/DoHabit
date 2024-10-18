68f8953e9401920bfd1f968afd7608861edb361a
Nikolai A. <ilowen@ya.ru>
2024-10-18 16:53:17 +0800

    Refactor: Updated dbColors to depend on the selected theme

--- src/db/dbColors.js
+++
@@ -1,8 +0,0 @@
-const colors = [];
-const colorsCount = 21;
-
-for (let i = 0; i < colorsCount; i++) {
-	colors.push(`hsl(${Math.floor(360 / colorsCount * i)}, 40%, 50%)`);
-};
-
-export default colors;
