236ddf08af9bb7746888f033574c9fd881429d34
Nikolai A. <ilowen@ya.ru>
2024-10-23 20:07:02 +0800

    Refactor: Remove one validation check in validateModalProps function

--- src/utils/validateModalProps.js
+++ src/utils/validateModalProps.js
@@ -1,9 +1,6 @@
 function validateModalProps(props) {
 	if (!props.modalTitle) {
 		throw new Error('Please provide a \'modalTitle\' in the props');
-	}
-	else if (!props.modalContent) {
-		throw new Error('Please provide a \'modalContent\' in the props');
 	};
 }
 
