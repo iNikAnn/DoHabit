c60454b2d2df15da3f752012d9f59be211ccb164
Nikolai A. <ilowen@ya.ru>
2024-10-24 19:36:55 +0800

    Refactor: Use Link in MenuItem component

--- src/components/Menu/MenuItem.jsx
+++ src/components/Menu/MenuItem.jsx
@@ -1,46 +1,51 @@
 import styles from '../../css/MenuItem.module.css';
 
+// router
+import { Link } from 'react-router-dom';
+
 // icons
 import { IoIosArrowForward } from "react-icons/io";
 import { LuExternalLink } from "react-icons/lu";
 
-function MenuItem({ icon, iconColor, title, desc, onClick, arrow, link, other }) {
+function MenuItem({ icon, iconColor, title, desc, onClick, to, arrow, link, other }) {
 	return (
 		<li>
-			<button
-				className={styles.menuItem}
-				onClick={onClick}
-			>
-				{icon && (
-					<div style={{ color: iconColor }}>
-						{icon}
+			<Link to={to}>
+				<button
+					className={styles.menuItem}
+					onClick={onClick}
+				>
+					{icon && (
+						<div style={{ color: iconColor }}>
+							{icon}
+						</div>
+					)}
+
+					<div className={styles.textWrapper}>
+						<h3 className={styles.title}>
+							{title}
+						</h3>
+
+						<small className={styles.desc}>
+							{desc}
+						</small>
 					</div>
-				)}
 
-				<div className={styles.textWrapper}>
-					<h3 className={styles.title}>
-						{title}
-					</h3>
+					{arrow && (
+						<IoIosArrowForward />
+					)}
 
-					<small className={styles.desc}>
-						{desc}
-					</small>
-				</div>
+					{link && (
+						<LuExternalLink />
+					)}
 
-				{arrow && (
-					<IoIosArrowForward />
-				)}
-
-				{link && (
-					<LuExternalLink />
-				)}
-
-				{other && (
-					<div className={styles.other}>
-						{other}
-					</div>
-				)}
-			</button>
+					{other && (
+						<div className={styles.other}>
+							{other}
+						</div>
+					)}
+				</button>
+			</Link>
 		</li>
 	);
 }
