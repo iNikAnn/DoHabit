b5f115149ede61e6863ca45e3f4c8884809cc707
Nikolai A. <ilowen@ya.ru>
2024-10-31 22:19:31 +0800

    New: Added achievement initialization function

---
+++ src/utils/initAchievements.js
@@ -0,0 +1,30 @@
+// utils
+import getFromLocalStorage from './getFromLocalStorage';
+
+// db
+import dbAchievements from '../db/dbAchievements';
+
+function initAchievements() {
+	let storedAchievements = getFromLocalStorage('achievements', dbAchievements);
+
+	// Updates achievements to match the database,
+	// preserving the unlocked status from local storage
+	const updatedAchievements = dbAchievements.map(
+		(a) => {
+			const existingAchievement = storedAchievements.find((localA) => a.id === localA.id);
+
+			if (existingAchievement) {
+				return {
+					...a,
+					isUnlocked: existingAchievement.isUnlocked
+				};
+			};
+
+			return a;
+		}
+	);
+
+	return updatedAchievements;
+}
+
+export default initAchievements;
