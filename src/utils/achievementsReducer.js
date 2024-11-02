6feefa2efe0efeea43f0c2030047155b4fa38329
Nikolai A. <ilowen@ya.ru>
2024-11-02 22:40:32 +0800

    New: Added achievementsReducer function

---
+++ src/utils/achievementsReducer.js
@@ -0,0 +1,96 @@
+// utils
+import saveToLocalStorage from './saveToLocalStorage';
+import getStreaks from './getStreaks';
+import getCompletionGaps from './getCompletionGaps';
+
+function achievementsReducer(achievements, actions) {
+	const { habits, onOpenDialog, isInitialRender } = actions;
+
+	const handleUnlockAchievement = (achievement) => {
+		achievement.isUnlocked = true;
+		achievement.unlockDate = new Date();
+
+		if (!isInitialRender) {
+			onOpenDialog({
+				title: 'Achievement Unlocked!',
+				imgSrc: `${process.env.PUBLIC_URL}/img/achievements/${achievement.id}.svg`,
+				text: `${achievement.title}\n${achievement.desc}`
+			});
+		};
+	};
+
+	achievements = achievements.map(
+		(a) => {
+			if (a.isUnlocked) return a;
+
+			const updatedA = { ...a };
+			let shouldUnlock = false;
+
+			switch (a.id) {
+				case 0:
+				case 1:
+				case 2:
+				case 3:
+				case 4:
+				case 5: {
+					const streaks = [];
+					for (const h of habits) {
+						const { longestStreak } = getStreaks(h.completedDays, h.frequency);
+						streaks.push(longestStreak);
+					};
+
+					shouldUnlock = streaks.some((s) => s >= a.criteria.streak);
+				};
+					break;
+
+				case 6: {
+					const gaps = [];
+					for (const h of habits) {
+						gaps.push(...getCompletionGaps(h.completedDays, h.frequency));
+					};
+
+					shouldUnlock = Math.max(...gaps) >= a.criteria.gap;
+				};
+					break;
+
+				case 7: {
+					const gaps = [];
+					for (const h of habits) {
+						gaps.push(...getCompletionGaps(h.completedDays, h.frequency));
+					};
+
+					shouldUnlock = gaps.includes(a.criteria.gap);
+				};
+					break;
+
+				default:
+					break;
+			};
+
+			if (shouldUnlock) handleUnlockAchievement(updatedA);
+
+			return updatedA;
+		}
+	);
+
+	// Save only basic info of unlocked achievements to local storage,
+	// full details will be stored in state from
+	// the achievements database during initialization
+	const basicAchievementsInfo = achievements
+		.filter(
+			(a) => a.isUnlocked
+		)
+		.map(
+			(a) => ({
+				id: a.id,
+				isUnlocked: a.isUnlocked,
+				unlockDate: a.unlockDate
+			})
+		);
+
+	saveToLocalStorage('achievements', basicAchievementsInfo);
+
+	return achievements;
+}
+
+export default achievementsReducer;
