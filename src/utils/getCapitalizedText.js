f7b5de4b7d9f9054ae10a767b62954a4e46e61f6
Nikolai A. <ilowen@ya.ru>
2024-12-23 21:33:43 +0800

    Refactor: Add JSDoc comments for getCapitalizedText function

--- src/utils/getCapitalizedText.js
+++ src/utils/getCapitalizedText.js
@@ -1,4 +1,16 @@
+/**
+ * Capitalizes the first letter of each word in the given text.
+ *
+ * @param {string} text - The input text to be capitalized.
+ * @returns {string} - The text with the first letter of each word capitalized.
+ * @throws {TypeError} - If the input is not a string.
+ */
+
 function getCapitalizedText(text) {
+	if (typeof text !== 'string') {
+		throw new TypeError('Input must be a string.');
+	};
+
 	return text.replace(/\b\w/g, (l) => l.toUpperCase());
 }
 
