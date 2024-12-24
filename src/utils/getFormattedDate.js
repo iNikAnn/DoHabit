7c734682466dbba1aeda2515e12c8d0bd9434202
Nikolai A. <ilowen@ya.ru>
2024-12-24 12:27:15 +0800

    Refactor: Add JSDoc comments for getFormattedDate function

--- src/utils/getFormattedDate.js
+++ src/utils/getFormattedDate.js
@@ -1,9 +1,17 @@
-function getFormattedDate(dateObj) {
-	const year = dateObj.getFullYear();
-	const month = String(dateObj.getMonth() + 1).padStart(2, 0);
-	const day = String(dateObj.getDate()).padStart(2, 0);
+/**
+ * Formats a Date object into a string in the "YYYY-MM-DD" format.
+ *
+ * @param {Date} dateObj - The Date object to format.
+ * @returns {string} - The date in "YYYY-MM-DD" format.
+ * @throws {TypeError} - If dateObj is not a Date object.
+ */
 
-	return year + '-' + month + '-' + day;
+function getFormattedDate(dateObj) {
+	if (!(dateObj instanceof Date)) {
+		throw new TypeError('Argument must be a Date object.');
+	};
+
+	return dateObj.toISOString().split('T')[0];
 }
 
 export default getFormattedDate;
