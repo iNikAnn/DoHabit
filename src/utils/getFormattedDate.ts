3a236529882ecb0e0ca4afbee2a576d635d124e5
Nikolai A. <ilowen@ya.ru>
2026-04-21 20:02:41 +0800

    refactor: migrate getFormattedDate to TS

---
+++ src/utils/getFormattedDate.ts
@@ -0,0 +1,12 @@
+/**
+ * Formats a Date object into a string in the "YYYY-MM-DD" format.
+ */
+function getFormattedDate(date: Date) {
+	const year = date.getFullYear();
+	const month = String(date.getMonth() + 1).padStart(2, '0');
+	const day = String(date.getDate()).padStart(2, '0');
+
+	return `${year}-${month}-${day}`;
+}
+
+export default getFormattedDate;
