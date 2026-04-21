a9def04d84517e8cfd9afafd51ee44475ea73559
Nikolai A. <ilowen@ya.ru>
2026-04-21 18:08:08 +0800

    feat: add types for habits

---
+++ src/types/habit.ts
@@ -0,0 +1,20 @@
+export interface Note {
+	text: string;
+	date: string;
+}
+
+export interface CompletedDay {
+	date: string;
+	progress: number;
+	isCompYdayBtnUsed: boolean;
+}
+
+export interface Habit {
+	title: string;
+	colorIndex: number;
+	iconTitle: string;
+	frequency: number;
+	creationDate: string;
+	completedDays: CompletedDay[];
+	diary: Note[];
+}
