3ccbb936c1843384027c7d8ac05b44676936fe49
Nikolai A. <ilowen@ya.ru>
2026-04-24 20:07:24 +0800

    refactor: migrate useAchievementsStore to TS

--- src/types/achievement.ts
+++ src/types/achievement.ts
@@ -1,3 +1,5 @@
+import { Habit, Note } from './habit';
+
 export interface Achievement {
 	id: number;
 	title: string;
@@ -12,4 +14,16 @@ export interface Achievement {
 	isSecret: boolean;
 	isUnlocked?: boolean;
 	unlockDate?: string;
+}
+
+export interface AchievementContext {
+	habits: Habit[];
+	mainDiary: Note[];
+	onUnlock: (ach: Achievement) => void;
+	isInitialRender: boolean;
+}
+
+export interface AchievementState {
+	achievements: Achievement[];
+	achievementsDispatch: (context: AchievementContext) => void;
 }
