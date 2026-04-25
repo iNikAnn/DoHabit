31b7bfd496da1b6cf997880691dc40ce22ffe9a6
Nikolai A. <ilowen@ya.ru>
2026-04-25 19:15:30 +0800

    feat: add docs and default values to mapHabitData

--- src/utils/mapHabitData.ts
+++ src/utils/mapHabitData.ts
@@ -1,10 +1,13 @@
 import { HabitFormData } from '../types/habit';
 
+/**
+ * Map raw form data to habit properties.
+ */
 function mapHabitData(data: HabitFormData) {
 	return {
 		title: data.title.value,
-		frequency: Number(data.frequency.value),
-		colorIndex: Number(data.colorIndex.value),
+		frequency: Number(data.frequency.value) || 1,
+		colorIndex: Number(data.colorIndex.value) || 0,
 		iconTitle: data.iconTitle.value
 	};
 }
