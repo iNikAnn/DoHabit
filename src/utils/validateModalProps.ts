91f991f4d05b6c00165caf465b11fed0263407c2
Nikolai A. <ilowen@ya.ru>
2026-04-26 21:31:02 +0800

    refactor: migrate validateModalProps to TS

--- src/utils/validateModalProps.ts
+++ src/utils/validateModalProps.ts
@@ -1,7 +1,11 @@
-function validateModalProps(props) {
+interface Props {
+	modalTitle: string;
+}
+
+function validateModalProps(props: Props) {
 	if (!props.modalTitle) {
 		throw new Error('Please provide a \'modalTitle\' in the props');
-	};
+	}
 }
 
 export default validateModalProps;
