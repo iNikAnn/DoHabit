2f67233cabd480c1a9a17b9b38204ccd3d71e479
Nikolai A. <ilowen@ya.ru>
2026-04-26 11:57:38 +0800

    feat: add types for settings

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,11 +1,5 @@
 export type ColorScheme = 'light' | 'dark';
 
-export interface Settings {
-	calendarView?: 'default' | 'compact';
-	calendarHighlightToday?: boolean;
-	isDarkSchemeForced?: boolean;
-}
-
 export interface Streak {
 	length: number;
 	start: string;
