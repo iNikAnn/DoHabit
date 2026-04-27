17332ffcb224844f7c64e93495783b01719b2b6e
Nikolai A. <ilowen@ya.ru>
2026-04-27 18:08:24 +0800

    refactor: migrate TextButton to TS

--- src/components/Actions/TextButton.tsx
+++ src/components/Actions/TextButton.tsx
@@ -3,7 +3,12 @@ import styles from '../../css/TextButton.module.css';
 // utils
 import { startCase } from 'es-toolkit';
 
-function TextButton({ text = 'Click me', onClick }) {
+interface Props {
+	text: string;
+	onClick: (...args: any) => void;
+}
+
+function TextButton({ text = 'Click me', onClick }: Props) {
 	return (
 		<button
 			className={styles.btn}
