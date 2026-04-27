b9281968c806feaa9d738bc48a2b5d95c97fb0f0
Nikolai A. <ilowen@ya.ru>
2026-04-27 15:14:13 +0800

    refactor: migrate HabitCard to TS

--- src/components/Habit/HabitCard.tsx
+++ src/components/Habit/HabitCard.tsx
@@ -15,6 +15,9 @@ import Calendar from './Calendar';
 import CompactCalendar from './CompactCalendar';
 import HabitMenu from './HabitMenu';
 
+// types
+import { Habit } from '../../types/habit';
+
 // utils
 import getColorPalette from '../../utils/getColorPalette';
 import getTodayProgress from '../../utils/getTodayProgress';
@@ -27,32 +30,54 @@ const today = new Date();
 const yesterday = new Date(today);
 yesterday.setDate(today.getDate() - 1);
 
-function Habit(props) {
+interface Props {
+	habit: Habit;
+	habitIndex: number;
+	color: string;
+	isArchive: boolean;
+	isMenuVisible: boolean;
+	onShowMenu: (i: number) => void;
+}
+
+function HabitCard(props: Props) {
 	const {
-		index, color, completedDays, frequency,
-		isMenuVisible, isArchive,
+		habit,
+		habitIndex,
+		color,
+		isArchive,
+		isMenuVisible,
 		onShowMenu
 	} = props;
 
 	const settings = useSettingsStore((s) => s.settings);
 	const habitRef = useRef(null);
 	const colorPalette = useMemo(() => getColorPalette(color), [color]);
-	const todayProgress = getTodayProgress(completedDays);
-	const { currentStreak } = getStreaks(completedDays, frequency);
+	const todayProgress = getTodayProgress(habit.completedDays);
+	const { currentStreak } = getStreaks(habit.completedDays, habit.frequency);
 
 	const [
 		isTodayCompleted,
 		isYesterdayCompleted
 	] = useMemo(
-		() => checkHabitCompletion(completedDays, frequency, today, yesterday),
-		[completedDays, frequency]
+		() => checkHabitCompletion(habit.completedDays, habit.frequency, today, yesterday),
+		[habit.completedDays, habit.frequency]
 	);
 
-	const handleShare = () => shareHabit(habitRef.current);
+	const handleShare = () => {
+		if (habitRef.current) {
+			shareHabit(habitRef.current);
+		} else {
+			alert('Something went wrong.');
+		}
+	};
 
 	const calendar = useMemo(
 		() => {
-			const props = { colorPalette, completedDays, frequency };
+			const props = {
+				colorPalette,
+				completedDays: habit.completedDays,
+				frequency: habit.frequency
+			};
 
 			return settings.calendarView === 'compact' ? (
 				<CompactCalendar {...props} />
@@ -60,7 +85,7 @@ function Habit(props) {
 				<Calendar {...props} />
 			);
 		},
-		[colorPalette, completedDays, frequency, settings.calendarView]
+		[colorPalette, habit.completedDays, habit.frequency, settings.calendarView]
 	);
 
 	const habitVariants = getListAnimationVariants(0.3);
@@ -71,11 +96,15 @@ function Habit(props) {
 			className={styles.habit}
 			{...habitVariants}
 			layout
-			onClick={() => onShowMenu(index)}
+			onClick={() => onShowMenu(habitIndex)}
 		>
 			<HabitHeader
-				{...{ ...props, colorPalette }}
-				{...{ isTodayCompleted, todayProgress, currentStreak }}
+				habit={habit}
+				colorPalette={colorPalette}
+				isTodayCompleted={!!isTodayCompleted}
+				todayProgress={todayProgress}
+				currentStreak={currentStreak}
+				isArchive={isArchive}
 			/>
 
 			{!isArchive && (
@@ -84,12 +113,17 @@ function Habit(props) {
 				</div>
 			)}
 
+			{/* @ts-ignore */}
 			<AnimatePresence>
 				{(isMenuVisible && !isArchive) && (
 					<HabitMenu
-						key="habitMenu"
-						{...props}
-						{...{ colorPalette, isTodayCompleted, isYesterdayCompleted, todayProgress, currentStreak }}
+						key='habitMenu'
+						habit={habit}
+						colorPalette={colorPalette}
+						isTodayCompleted={!!isTodayCompleted}
+						isYesterdayCompleted={!!isYesterdayCompleted}
+						todayProgress={todayProgress}
+						currentStreak={currentStreak}
 						onShowMenu={onShowMenu}
 						onShare={handleShare}
 					/>
@@ -99,4 +133,4 @@ function Habit(props) {
 	);
 }
 
-export default Habit;
+export default HabitCard;
