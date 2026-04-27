8cf18045549f5f71bc51c51ab39de011afc6b971
Nikolai A. <ilowen@ya.ru>
2026-04-27 18:57:58 +0800

    refactor: migrate WeekdayChart to TS

--- src/components/Statistics/WeekdayChart.tsx
+++ src/components/Statistics/WeekdayChart.tsx
@@ -1,24 +1,37 @@
 // constants
 import { WEEKDAYS } from '../../constants';
 
+// types
+import { CompletedDay } from '../../types/habit';
+
 // utils
 import getCompletionCountPerDay from '../../utils/getCompletionCountPerDay';
 
 // chart js
-import { BarElement, Chart as ChartJS } from "chart.js";
-import { Bar } from "react-chartjs-2";
+import { BarElement, Chart as ChartJS, ChartData, ChartOptions } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 
 ChartJS.register(BarElement);
 
-function WeekdayChart({ options, days, frequency, color }) {
+interface Props {
+	options: ChartOptions<'bar'>;
+	days: CompletedDay[];
+	frequency: number;
+	color: string;
+}
+
+function WeekdayChart({ options, days, frequency, color }: Props) {
 	const data = getCompletionCountPerDay(days, frequency);
 
-	const config = {
+	const config: {
+		data: ChartData<'bar'>,
+		options: ChartOptions<'bar'>
+	} = {
 		data: {
 			labels: [...WEEKDAYS.slice(1), 'Sun'],
 			datasets: [{
 				label: 'WeekdayChart',
-				data: [...data.slice(1), data[0]],
+				data: [...data.slice(1), data[0]!],
 
 				backgroundColor: color,
 				borderRadius: 10,
--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,9 @@
 export const WEEKDAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'] as const;
 
+export const MONTHS = [
+	'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+	'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+] as const;
+
 /** Number of milliseconds in one day. */
 export const DAY_MS = 24 * 60 * 60 * 1000; // 86_400_000
