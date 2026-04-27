fff779ab30134a0c7bb7d8ebb438f9153ce4128b
Nikolai A. <ilowen@ya.ru>
2026-04-27 11:54:36 +0800

    refactor: migrate HabitList to TS

--- src/components/HabitList.tsx
+++ src/components/HabitList.tsx
@@ -1,7 +1,7 @@
 import styles from '../css/HabitList.module.css';
 
 // components
-import Habit from "./Habit/Habit";
+import HabitCard from "./Habit/HabitCard";
 
 // react
 import { useState } from 'react';
@@ -12,30 +12,41 @@ import { AnimatePresence } from 'framer-motion';
 // stores
 import { useColorsStore } from '../stores/colorsStore';
 
-function HabitList({ habits, isArchive = false }) {
+// types
+import { Habit } from '../types/habit';
+
+interface Props {
+	habits: Habit[];
+	isArchive?: boolean;
+}
+
+function HabitList({ habits, isArchive = false }: Props) {
 
 	const dbColors = useColorsStore((s) => s.colors);
 	const [visibleMenuIndex, setVisibleMenuIndex] = useState(-1);
 
-	const handleToggleMenu = (i) => {
+	const handleToggleMenu = (i: number) => {
 		document.body.classList.toggle('no-scroll');
 		setVisibleMenuIndex(i === visibleMenuIndex ? -1 : i);
 	};
 
 	const habitList = habits.map(
-		(h, index) => (
-			<Habit
-				key={h.title}
-				{...{ ...h, index, isArchive }}
-				color={dbColors[h.colorIndex]}
+		(habit, index) => (
+			<HabitCard
+				key={habit.title}
+				habit={habit}
+				habitIndex={index}
+				color={dbColors[habit.colorIndex] ?? 'red'}
+				isArchive={isArchive}
 				isMenuVisible={visibleMenuIndex === index}
-				onShowMenu={(i) => handleToggleMenu(i)}
+				onShowMenu={handleToggleMenu}
 			/>
 		)
 	);
 
 	return (
 		<div className={styles.habitList}>
+			{/* @ts-ignore */}
 			<AnimatePresence initial={false}>
 				{habitList}
 			</AnimatePresence>
