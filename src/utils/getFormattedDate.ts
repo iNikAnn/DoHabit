6bb1fb210a9c31285a8f01a72fe4c582fa985bc9
Nikolai A. <ilowen@ya.ru>
2026-04-28 15:46:58 +0800

    feat: add optional time to getFormattedDate

--- src/utils/getFormattedDate.ts
+++ src/utils/getFormattedDate.ts
@@ -1,12 +1,32 @@
 /**
- * Formats a Date object into a string in the "YYYY-MM-DD" format.
+ * Formats a Date object into "YYYY-MM-DD" or "YYYY-MM-DD HH:mm".
  */
-function getFormattedDate(date: Date) {
+
+interface Options {
+	includeTime?: boolean;
+}
+
+function getFormattedDate(date: Date, options?: Options): string {
+	const {
+		includeTime
+	} = options ?? {};
+
 	const year = date.getFullYear();
 	const month = String(date.getMonth() + 1).padStart(2, '0');
 	const day = String(date.getDate()).padStart(2, '0');
 
-	return `${year}-${month}-${day}`;
+	const datePart = `${year}-${month}-${day}`;
+
+	if (includeTime) {
+		const timePart = date.toLocaleTimeString('en-GB', {
+			hour: '2-digit',
+			minute: '2-digit'
+		});
+
+		return `${datePart} ${timePart}`;
+	}
+
+	return datePart;
 }
 
 export default getFormattedDate;
