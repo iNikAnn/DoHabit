b8d716afca06b154f3d644a92fa0607fb07101dc
Nikolai A. <ilowen@ya.ru>
2026-05-04 09:19:05 +0800

    docs: add comments do AppRouter

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -2,12 +2,20 @@ import { AnimatePresence } from 'framer-motion';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import { routeConfig } from '../config/routeConfig';
 
+/**
+ * Main application router with transition animations.
+ * Renders routes from routeConfig.
+ */
 function AppRouter() {
 	const location = useLocation();
 
 	return (
 		// @ts-ignore
 		<AnimatePresence initial={false}>
+			{/*
+                key & location are required for AnimatePresence
+                to detect route changes and trigger exit animations.
+            */}
 			<Routes key={location.pathname} location={location}>
 				{routeConfig.map(({ path, element, children }) => (
 					<Route
@@ -15,6 +23,7 @@ function AppRouter() {
 						path={path}
 						element={element}
 					>
+						{/* Nested routes for ModalLayout */}
 						{children?.map(({ path, element }) => (
 							<Route
 								key={path}
