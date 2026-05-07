ed1b551a12e0e78400c470df506e38bc39456e10
Nikolai A. <ilowen@ya.ru>
2026-05-07 21:53:36 +0800

    refactor: simplify HabitCard logic by moving helpers out

--- src/entities/habit/ui/habit-card/HabitCard.tsx
+++ src/entities/habit/ui/habit-card/HabitCard.tsx
@@ -1,78 +1,34 @@
 import styles from './HabitCard.module.css';
-
-// react
-import { CSSProperties, ReactNode, useMemo, useRef } from 'react';
-
-// framer
+import { CSSProperties, ReactNode, useMemo } from 'react';
 import { motion } from 'framer-motion';
-
-// stores
 import { useSettingsStore } from '../../../../stores/settingsStore';
-
-// components
 import HabitHeader from '../habit-header/HabitHeader';
 import Calendar from '../../../../components/Habit/Calendar';
 import CompactCalendar from '../../../../components/Habit/CompactCalendar';
-
-// types
 import { Habit } from '../../model/types';
-
-// utils
-import { getColorVariants, shareElementScreenshot } from '@shared/lib';
-import { getStreaks } from '../../lib/getStreaks';
-import { checkHabitCompletion } from '../../lib/checkHabitCompletion';
 import getListAnimationVariants from '../../../../utils/getListAnimationVariants';
-import { DrawerAction, useDrawerStore } from '@shared/ui';
-import { MdEditSquare, MdLibraryBooks } from 'react-icons/md';
-import { getModalPath } from '@shared/const';
-import { FaCalendarCheck, FaCalendarTimes, FaShareAltSquare } from 'react-icons/fa';
-import { FaChartSimple } from 'react-icons/fa6';
-import { useHabitsStore } from '../../model/store';
-import { getTodayProgress } from '@entities/habit';
-
-const today = new Date();
-const yesterday = new Date(today);
-yesterday.setDate(today.getDate() - 1);
+import { ColorVariants } from '@/types/colorScheme';
 
 interface HabitCardProps {
 	headerAction?: ReactNode;
 	habit: Habit;
-	color: string;
+	colorVariants: ColorVariants;
+	currentStreak: number;
 	isArchive: boolean;
+	onClick: (e: React.MouseEvent<HTMLDivElement>) => void;
 }
 
 function HabitCard(props: HabitCardProps) {
 	const {
 		headerAction,
 		habit,
-		color,
+		colorVariants,
+		currentStreak,
 		isArchive,
+		onClick
 	} = props;
 
-	const habitsDispatch = useHabitsStore((s) => s.habitsDispatch);
 	const settings = useSettingsStore((s) => s.settings);
-	const habitRef = useRef(null);
-	const colorVariants = useMemo(() => getColorVariants(color), [color]);
-	const {
-		progress: todayProgress,
-		isCompleted: isTodayCompleted
-	} = getTodayProgress({ completedDays: habit.completedDays, frequency: habit.frequency });
-	const { currentStreak } = getStreaks(habit.completedDays, habit.frequency);
-
-	const [
-		isYesterdayCompleted
-	] = useMemo(
-		() => checkHabitCompletion(habit.completedDays, habit.frequency, today, yesterday),
-		[habit.completedDays, habit.frequency]
-	);
-
-	const handleShareHabit = () => {
-		if (habitRef.current) {
-			shareElementScreenshot(habitRef.current);
-		} else {
-			alert('Something went wrong.');
-		}
-	};
 
 	const calendar = useMemo(
 		() => {
@@ -92,99 +48,20 @@ function HabitCard(props: HabitCardProps) {
 	);
 
 	const habitVariants = getListAnimationVariants(0.3);
-	const { baseColor, darkenedColor, softenedColor } = colorVariants;
-
-	const openDrawer = useDrawerStore((s) => s.open);
-
-	const handleCompleteYeserday = () => {
-		habitsDispatch({
-			type: 'toggleYesterdayStatus',
-			payload: {
-				habitId: habit.title,
-				isTodayCompleted,
-				isYesterdayCompleted,
-				todayProgress
-			}
-		});
-	};
-
-	const handleOpenDrawer = () => {
-		if (isArchive) return;
-
-		const actions: DrawerAction[] = [
-			{
-				icon: isYesterdayCompleted ? <FaCalendarTimes /> : <FaCalendarCheck />,
-				label: (isYesterdayCompleted ? 'Uncomp.' : 'Comp.') + ' Y\'day',
-				style: { backgroundColor: isYesterdayCompleted ? 'IndianRed' : darkenedColor },
-				onClick: handleCompleteYeserday
-			},
-			{
-				to: getModalPath('HABIT_EDITOR'),
-				state: {
-					habitId: habit.id,
-					modalTitle: 'Edit habit',
-				},
-				icon: <MdEditSquare />,
-				label: 'Edit Habit',
-				indicator: { type: 'arrow' },
-				style: { backgroundColor: darkenedColor }
-			},
-			{
-				icon: <FaShareAltSquare />,
-				label: 'Share Habit',
-				onClick: handleShareHabit,
-				style: { backgroundColor: darkenedColor }
-			},
-			{
-				to: getModalPath('STATISTICS'),
-				state: {
-					completedDays: habit.completedDays,
-					colorVariants,
-					colorIndex: habit.colorIndex,
-					frequency: habit.frequency,
-					modalTitle: habit.title,
-				},
-				icon: <FaChartSimple />,
-				label: 'Statistics',
-				indicator: { type: 'arrow' },
-				style: { backgroundColor: darkenedColor }
-			},
-			{
-				to: getModalPath('DIARY'),
-				state: {
-					currentStreak,
-					habitTitle: habit.title,
-					colorIndex: habit.colorIndex,
-					modalTitle: habit.title,
-				},
-				icon: <MdLibraryBooks />,
-				label: 'Diary',
-				indicator: { type: 'arrow' },
-				style: { backgroundColor: darkenedColor }
-			}
-		];
-
-		openDrawer({
-			title: habit.title,
-			actions
-		});
-	};
 
 	return (
 		<motion.div
-			ref={habitRef}
-
 			// Inject dynamic colors as CSS variables.
 			style={{
-				'--habit-color-base': baseColor,
-				'--habit-color-dark': darkenedColor,
-				'--habit-color-soft': softenedColor
+				'--habit-color-base': colorVariants.baseColor,
+				'--habit-color-dark': colorVariants.darkenedColor,
+				'--habit-color-soft': colorVariants.softenedColor
 			} as CSSProperties}
 
 			className={styles.habit}
 			{...habitVariants}
 			layout
-			onClick={handleOpenDrawer}
+			onClick={onClick}
 		>
 			<HabitHeader
 				action={headerAction}
--- src/widgets/habit-list/ui/HabitList.tsx
+++ src/widgets/habit-list/ui/HabitList.tsx
@@ -1,13 +1,18 @@
 import styles from './HabitList.module.css';
 import { AnimatePresence } from 'framer-motion';
 import { useColorsStore } from '../../../stores/colorsStore';
-import { MdAddToPhotos } from 'react-icons/md';
+import { MdAddToPhotos, MdEditSquare, MdLibraryBooks } from 'react-icons/md';
+import { getHabitStats } from '../lib/getHabitStats';
 import { UpdateHabitProgress } from '@features/update-habit-progress';
 import { RestoreHabit } from '@features/restore-habit';
-import { HabitCard, useHabitsStore } from '@entities/habit';
+import { Habit, HabitCard, useHabitsStore } from '@entities/habit';
 import { CalendarIcon, TableIcon } from '@shared/assets';
 import { getModalPath } from '@shared/const';
-import { Placeholder } from '@shared/ui';
+import { DrawerAction, Placeholder, useDrawerStore } from '@shared/ui';
+import { getColorVariants, shareElementScreenshot } from '@shared/lib';
+import { FaCalendarCheck, FaCalendarTimes, FaShareAltSquare } from 'react-icons/fa';
+import { ColorVariants } from '@/types/colorScheme';
+import { FaChartSimple } from 'react-icons/fa6';
 
 interface HabitListParams {
 	isArchive?: boolean;
@@ -23,7 +28,9 @@ function HabitList(params: HabitListParams) {
 	} = params;
 
 	const habits = useHabitsStore((s) => s.habits);
+	const habitsDispatch = useHabitsStore((s) => s.habitsDispatch);
 	const colors = useColorsStore((s) => s.colors);
+	const openDrawer = useDrawerStore((s) => s.open);
 
 	// Filter habits based on mode
 	const filteredHabits = habits.filter((h) => isArchive ? h.isArchived : !h.isArchived);
@@ -55,22 +62,120 @@ function HabitList(params: HabitListParams) {
 		);
 	}
 
+	const handleShareHabit = (element: HTMLDivElement) => {
+		if (element) {
+			shareElementScreenshot(element);
+		} else {
+			alert('Something went wrong.');
+		}
+	};
+
+	const handleCardClick = (habit: Habit, data: {
+		isYdayCompleted: boolean,
+		todayProgress: number,
+		currentStreak: number,
+		colorVariants: ColorVariants
+	}, target: EventTarget | null) => {
+		if (isArchive) return;
+
+		const {
+			isYdayCompleted,
+			todayProgress,
+			currentStreak,
+			colorVariants
+		} = data;
+
+		const { darkenedColor } = colorVariants;
+
+		const actions: DrawerAction[] = [
+			{
+				icon: isYdayCompleted ? <FaCalendarTimes /> : <FaCalendarCheck />,
+				label: (isYdayCompleted ? 'Uncomp.' : 'Comp.') + ' Y\'day',
+				style: { backgroundColor: isYdayCompleted ? 'IndianRed' : darkenedColor },
+				onClick: () => habitsDispatch({
+					type: 'toggleYesterdayStatus',
+					payload: {
+						habitId: habit.id,
+						isYdayCompleted,
+						todayProgress
+					}
+				})
+			},
+			{
+				to: getModalPath('HABIT_EDITOR'),
+				state: {
+					habitId: habit.id,
+					modalTitle: 'Edit habit',
+				},
+				icon: <MdEditSquare />,
+				label: 'Edit Habit',
+				indicator: { type: 'arrow' },
+				style: { backgroundColor: darkenedColor }
+			},
+			{
+				icon: <FaShareAltSquare />,
+				label: 'Share Habit',
+				onClick: () => handleShareHabit(target as HTMLDivElement),
+				style: { backgroundColor: darkenedColor }
+			},
+			{
+				to: getModalPath('STATISTICS'),
+				state: {
+					completedDays: habit.completedDays,
+					colorVariants,
+					colorIndex: habit.colorIndex,
+					frequency: habit.frequency,
+					modalTitle: habit.title,
+				},
+				icon: <FaChartSimple />,
+				label: 'Statistics',
+				indicator: { type: 'arrow' },
+				style: { backgroundColor: darkenedColor }
+			},
+			{
+				to: getModalPath('DIARY'),
+				state: {
+					currentStreak,
+					habitTitle: habit.title,
+					colorIndex: habit.colorIndex,
+					modalTitle: habit.title,
+				},
+				icon: <MdLibraryBooks />,
+				label: 'Diary',
+				indicator: { type: 'arrow' },
+				style: { backgroundColor: darkenedColor }
+			}
+		];
+
+		openDrawer({
+			title: habit.title,
+			actions
+		});
+	};
+
 	// 2. Render list
 	return (
 		<div className={styles.habitList}>
 			{/* @ts-ignore */}
 			<AnimatePresence initial={false}>
-				{filteredHabits.map((habit) => (
-					<HabitCard
-						key={habit.id}
-						headerAction={isArchive
-							? <RestoreHabit habitId={habit.id} />
-							: <UpdateHabitProgress habit={habit} />}
-						habit={habit}
-						color={colors[habit.colorIndex] ?? 'red'}
-						isArchive={isArchive}
-					/>
-				))}
+				{filteredHabits.map((habit) => {
+					const colorVariants = getColorVariants(colors[habit.colorIndex]);
+					const stats = getHabitStats(habit);
+
+					return (
+						<HabitCard
+							key={habit.id}
+							headerAction={isArchive
+								? <RestoreHabit habitId={habit.id} />
+								: <UpdateHabitProgress habit={habit} />}
+							habit={habit}
+							colorVariants={colorVariants}
+							currentStreak={stats.currentStreak}
+							isArchive={isArchive}
+							onClick={(e) => handleCardClick(habit, { colorVariants, ...stats }, e.currentTarget)}
+						/>
+					);
+				})}
 			</AnimatePresence>
 		</div>
 	);
