45f1459f3881574a4ec28c336abc38e74baa53ae
Nikolai A. <ilowen@ya.ru>
2026-05-12 13:05:01 +0800

    refactor: optimize getCompletedDatesSet

--- src/entities/habit/lib/getCompletedDatesSet.ts
+++ src/entities/habit/lib/getCompletedDatesSet.ts
@@ -1,5 +1,6 @@
 import { CompletedDay } from '../model/types';
 import { formatDate } from '@shared/lib';
+import { removeIncompleteLatestDay } from './removeIncompleteLatestDay';
 
 /**
  * Resolves completed dates into a Set.
@@ -9,22 +10,28 @@ function getCompletedDatesSet(
 	frequency: number,
 	...dates: Date[]
 ): Set<string> {
-	// Create index of all valid completions
-	const completedSet = new Set(
-		completedDays
-			.filter((d) => d.progress === undefined || d.progress >= frequency)
-			.map((d) => d.date)
-	);
-
 	const result = new Set<string>();
+	if (completedDays.length === 0 || dates.length === 0) return result;
 
-	dates.forEach((date) => {
-		const formattedDate = formatDate(date);
+	// Filter out latest day if it's not finished yet
+	const sanitizedDays = removeIncompleteLatestDay(completedDays, frequency);
 
-		if (completedSet.has(formattedDate)) {
-			result.add(formattedDate);
+	// Prepare targets and find the oldest one to set a deadline
+	const targetDateStrings = dates.map((d) => formatDate(d));
+	const oldestTarget = targetDateStrings.reduce((a, b) => a < b ? a : b);
+	const targetSet = new Set(targetDateStrings);
+
+	for (const entry of sanitizedDays) {
+		// All targets found. Stop.
+		if (result.size === targetSet.size) break;
+
+		// Current entry is older than oldest target. Stop.
+		if (entry.date < oldestTarget) break;
+
+		if (targetSet.has(entry.date)) {
+			result.add(entry.date);
 		}
-	});
+	}
 
 	return result;
 }
