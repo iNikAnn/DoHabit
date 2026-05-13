42b96a9969026c6a55e2eb3b01cf9921a862b041
Nikolai A. <ilowen@ya.ru>
2026-05-13 16:20:51 +0800

    fix: use Math.round and normalization in countDaysBetween

--- src/shared/lib/utils/count-days-between/countDaysBetween.ts
+++ src/shared/lib/utils/count-days-between/countDaysBetween.ts
@@ -4,8 +4,11 @@ import { DAY_MS } from '@shared/const';
  * Calculates the number of full days between two dates,
  * excluding the start and end days.
  */
-function countDaysBetween(dateObj1: Date, dateObj2: Date): number {
-	return Math.abs((dateObj1.getTime() - dateObj2.getTime()) / DAY_MS) - 1;
+function countDaysBetween(d1: Date, d2: Date): number {
+	const start = new Date(d1.getFullYear(), d1.getMonth(), d1.getDate());
+	const end = new Date(d2.getFullYear(), d2.getMonth(), d2.getDate());
+
+	return Math.round(Math.abs(start.getTime() - end.getTime()) / DAY_MS);
 }
 
 export { countDaysBetween };
