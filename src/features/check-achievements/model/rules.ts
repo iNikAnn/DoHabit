beda1506319b55f52052c666e86db9b55e1162fe
Nikolai A. <ilowen@ya.ru>
2026-05-16 12:06:37 +0800

    fead: add perfect-year achievement rule

--- src/features/check-achievements/model/rules.ts
+++ src/features/check-achievements/model/rules.ts
@@ -1,12 +1,13 @@
 import { CheckContext } from './types';
 import { AchievementId } from '@entities/achievement';
+import { getStreaks } from '@entities/habit';
 import { formatDate } from '@shared/lib';
 
 export const achievementRules: Record<AchievementId, (ctx: CheckContext) => boolean> = {
 	'fresh-start': ({ habits }) => habits.length > 0,
 
 	'new-years-resolution': ({ habits }) => habits.some((h) => {
-		if (!h.createdAt || !h.completedDays.length) return false;
+		if (!h.createdAt || h.completedDays.length === 0) return false;
 
 		const creationDate = (new Date(h.createdAt));
 
@@ -17,5 +18,22 @@ export const achievementRules: Record<AchievementId, (ctx: CheckContext) => bool
 		const firstCompletedDay = h.completedDays.at(-1);
 
 		return firstCompletedDay?.date === janFirstStr;
-	})
+	}),
+
+	'perfect-year': (({ habits }) => habits.some((h) => {
+		// Fast exit: if total completed days are less than a year
+		if (!h.completedDays || h.completedDays.length < 365) return false;
+
+		const { allStreaks } = getStreaks(h.completedDays);
+
+		return allStreaks.some((streak) => {
+			// Skip small streaks
+			if (streak.length < 365) return false;
+
+			const startDate = new Date(streak.start);
+			const isStartedJanFirst = startDate.getDate() === 1 && startDate.getMonth() === 0;
+
+			return isStartedJanFirst;
+		});
+	}))
 };
