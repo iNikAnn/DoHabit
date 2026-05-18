badb93e4e6e624ec645467cb4bba7b5a2374c42c
Nikolai A. <ilowen@ya.ru>
2026-05-18 11:41:32 +0800

    refactor: move theme options config to useListItems hook

--- src/widgets/theme-settings/model/useThemeActions.ts
+++ src/widgets/theme-settings/model/useThemeActions.ts
@@ -4,12 +4,14 @@ import { useSettingsStore } from '@entities/settings';
 import { DrawerAction } from '@shared/ui';
 
 function useThemeActions() {
+	const settings = useSettingsStore((s) => s.settings);
 	const settingsDispatch = useSettingsStore((s) => s.settingsDispatch);
 
 	const themeActions: DrawerAction[] = [
 		{
 			icon: FaFont,
 			label: 'Auto',
+			indicator: { type: !settings.theme ? 'checkmark' : 'none' },
 			onClick: () => settingsDispatch({
 				type: 'updateSettings',
 				payload: { theme: undefined }
@@ -19,6 +21,7 @@ function useThemeActions() {
 			icon: FaSun,
 			iconProps: { color: '#e3bb44' },
 			label: 'Light',
+			indicator: { type: settings.theme === 'light' ? 'checkmark' : 'none' },
 			onClick: () => settingsDispatch({
 				type: 'updateSettings',
 				payload: { theme: 'light' }
@@ -28,6 +31,7 @@ function useThemeActions() {
 			icon: FaMoon,
 			iconProps: { color: '#5070b5' },
 			label: 'Dark',
+			indicator: { type: settings.theme === 'dark' ? 'checkmark' : 'none' },
 			onClick: () => settingsDispatch({
 				type: 'updateSettings',
 				payload: { theme: 'dark' }
--- src/widgets/theme-settings/ui/ThemeSettings.tsx
+++ src/widgets/theme-settings/ui/ThemeSettings.tsx
@@ -1,35 +1,12 @@
-import { FaFont } from 'react-icons/fa';
-import { FaMoon, FaSun } from 'react-icons/fa6';
-import useThemeActions from '../model/useThemeActions';
-import { useSettingsStore } from '@entities/settings';
-import { ListItemProps, List, useDrawerStore } from '@shared/ui';
+import useListItems from '../model/useListItems';
+import { List } from '@shared/ui';
 
 function ThemeSettings() {
-	const openDrawer = useDrawerStore((s) => s.open);
-	const settings = useSettingsStore((s) => s.settings);
-	const { themeActions } = useThemeActions();
-
-	const menuItems: ListItemProps[] = [
-		{
-			icon: settings.theme === 'light'
-				? <FaSun color='#e3bb44' />
-				: settings.theme === 'dark'
-					? <FaMoon color='#5070b5' />
-					: <FaFont />,
-			title: 'Theme',
-			description: `Current: ${settings.theme ?? 'auto'}`,
-			onClick: () => {
-				openDrawer({
-					title: 'Select theme',
-					actions: themeActions
-				})
-			}
-		}
-	];
+	const { listItems } = useListItems();
 
 	return (
 		<List
-			items={menuItems}
+			items={listItems}
 		/>
 	);
 }
