dc4011921f1a513d2df22a8cd7dd45dd0593001e
Nikolai A. <ilowen@ya.ru>
2026-05-20 22:34:27 +0800

    refactor: move WeekdayChart to widgets layer

---
+++ src/widgets/habit-stats/weekday-chart/index.ts
@@ -0,0 +1,1 @@
+export { WeekdayChart } from './ui/WeekdayChart'
--- src/widgets/habit-stats/weekday-chart/ui/WeekdayChart.tsx
+++ src/widgets/habit-stats/weekday-chart/ui/WeekdayChart.tsx
@@ -2,6 +2,8 @@ import { BarElement, Chart as ChartJS, ChartData, ChartOptions } from 'chart.js'
 import { Bar } from 'react-chartjs-2';
 import { CompletedDay, getCompletionCountPerDay } from '@entities/habit';
 import { WEEKDAYS } from '@shared/const';
+import { Card } from '@shared/ui';
+import { FaCalendarWeek } from 'react-icons/fa';
 
 ChartJS.register(BarElement);
 
@@ -11,8 +13,17 @@ interface Props {
 	color: string;
 }
 
+/**
+ * Renders a bar chart showing habit completion frequency for each day of the week.
+ */
 function WeekdayChart({ options, days, color }: Props) {
 	const data = getCompletionCountPerDay(days);
+
+	// Canvas API doesn't support CSS color functions, so resolve them manually here.
+	const tempDiv = document.createElement('div');
+	tempDiv.style.color = color;
+	document.body.append(tempDiv);
+	const computedColor = getComputedStyle(tempDiv).color;
 
 	const config: {
 		data: ChartData<'bar'>,
@@ -24,18 +35,22 @@ function WeekdayChart({ options, days, color }: Props) {
 				label: 'WeekdayChart',
 				data: [...data.slice(1), data[0]!],
 
-				backgroundColor: color,
+				backgroundColor: computedColor,
 				borderRadius: 10,
 				borderSkipped: false
 			}]
 		},
-
 		options
 	};
 
 	return (
-		<Bar {...config} />
+		<Card
+			title='Completions / Weekday'
+			extra={<FaCalendarWeek style={{ color }} />}
+		>
+			<Bar {...config} />
+		</Card>
 	);
 }
 
-export default WeekdayChart;
+export { WeekdayChart };
