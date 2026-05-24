24700e2fe4425ac39615929fe9d0fe4280b97638
Nikolai A. <ilowen@ya.ru>
2026-05-24 14:43:20 +0800

    feat: add stylelint package

---
+++ stylelint.config.ts
@@ -0,0 +1,10 @@
+/** @type {import("stylelint").Config} */
+export default {
+	extends: [
+		'stylelint-config-standard',
+		'stylelint-config-recess-order'
+	],
+	plugins: [
+		'stylelint-order'
+	]
+};
