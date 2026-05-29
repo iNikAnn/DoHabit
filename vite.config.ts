6a4d0b6e13110ac44f11190b013dc8269ed5eb50
Nikolai A. <ilowen@ya.ru>
2026-05-29 18:45:18 +0800

    chore: remove .ico icon from manifest

--- vite.config.ts
+++ vite.config.ts
@@ -55,13 +55,6 @@ export default defineConfig({
 				],
 
 				icons: [
-					// favicon
-					{
-						src: 'assets/brand/favicon.ico',
-						sizes: "64x64 32x32 24x24 16x16",
-						type: "image/x-icon",
-					},
-
 					// 192
 					{
 						src: 'assets/brand/logo192-alpha.png',
