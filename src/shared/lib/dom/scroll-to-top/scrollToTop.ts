1ca6f3de03eb55302258156e0fccfc1c3df921f0
Nikolai A. <ilowen@ya.ru>
2026-06-02 11:14:37 +0800

    feat: add options param to scrollToTop

--- src/shared/lib/dom/scroll-to-top/scrollToTop.ts
+++ src/shared/lib/dom/scroll-to-top/scrollToTop.ts
@@ -1,12 +1,17 @@
+interface ScrollToTopOptions {
+	behavior?: 'auto' | 'instant' | 'smooth';
+}
+
 /**
  * Smoothly scrolls element or window to top.
  */
-function scrollToTop(element?: Element | null) {
+function scrollToTop(element?: Element | null, options?: ScrollToTopOptions) {
 	const target = element ?? window;
+	const { behavior = 'smooth' } = options ?? {};
 
 	target.scrollTo({
 		top: 0,
-		behavior: 'smooth'
+		behavior
 	});
 }
 
