4ebdfb4f17907db0cb09c1de07533bf2de53c29f
Nikolai A. <ilowen@ya.ru>
2026-06-04 19:21:15 +0800

    feat: add i18next.d.ts

--- src/shared/lib/i18n/index.ts
+++ src/shared/lib/i18n/index.ts
@@ -1,9 +1,1 @@
-import type { AppLanguageCode } from './types';
-
 export { resources, languageMap, SUPPORTED_LANGUAGES } from './config';
-
-declare module 'i18next' {
-	interface i18n {
-		language: AppLanguageCode;
-	}
-}
