15be86a7160f70778644ed66ed6ec8e91889c7da
Nikolai A. <ilowen@ya.ru>
2026-06-06 17:27:53 +0800

    refactor: remove manual language persistence from useLangActions

--- src/widgets/interface-settings/model/useLangActions.ts
+++ src/widgets/interface-settings/model/useLangActions.ts
@@ -1,22 +1,14 @@
-import { useSettingsStore } from '@entities/settings';
 import { type DrawerAction } from '@shared/ui';
 import { useTranslation } from 'react-i18next';
 import { SUPPORTED_LANGUAGES } from '@shared/lib/i18n';
 
 function useLangActions() {
-	const settingsDispatch = useSettingsStore((s) => s.settingsDispatch);
 	const { i18n } = useTranslation();
 
 	const langActions: DrawerAction[] = SUPPORTED_LANGUAGES.map((lang) => ({
 		label: lang.label,
 		indicator: { type: i18n.language === lang.code ? 'checkmark' : 'none' },
-		onClick: () => {
-			i18n.changeLanguage(lang.code);
-			settingsDispatch({
-				type: 'updateSettings',
-				payload: { language: lang.code }
-			})
-		}
+		onClick: () => i18n.changeLanguage(lang.code)
 	}));
 
 	return {
