bdbc17fa01c13109d8928612bc19d6fe58ff7d39
Nikolai A. <ilowen@ya.ru>
2026-06-06 17:33:55 +0800

    feat: add automatic browser language detection

--- src/app/providers/i18n/config/i18nConfig.ts
+++ src/app/providers/i18n/config/i18nConfig.ts
@@ -1,14 +1,20 @@
 import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 import { resources } from '@shared/lib/i18n';
 
 i18n
+	.use(LanguageDetector)
 	.use(initReactI18next)
 	.init({
+		supportedLngs: Object.keys(resources),
 		resources,
-		lng: 'en',
-		fallbackLng: 'en'
+		fallbackLng: 'en',
+		detection: {
+			// Extracts only the base language code (e.g., "en" from "en-US")
+			// before passing it to i18next and before saving to localStorage
+			convertDetectedLanguage: (lng) => lng.split('-')[0]
+		}
 	});
-
 
 export default i18n;
