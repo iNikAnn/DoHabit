36c044dbcf0d7cf3a45d782030d5b2dda2788bb7
Nikolai A. <ilowen@ya.ru>
2026-06-07 10:40:41 +0800

    feat: add long press support to NoteCard

--- src/entities/note/ui/NoteCard.tsx
+++ src/entities/note/ui/NoteCard.tsx
@@ -1,4 +1,6 @@
 import styles from './NoteCard.module.css';
+import { useRef } from 'react';
+import { useLongPress } from '@uidotdev/usehooks';
 import type { Note } from '../model/types';
 import { formatDate } from '@shared/lib/date-time';
 import NoteText from './note-text/NoteText';
@@ -6,6 +8,7 @@ import NoteText from './note-text/NoteText';
 interface NoteCardProps {
 	note: Note;
 	onCardClick: () => void;
+	onLongPress: (id: string) => void;
 	onTagClick: (tag: string) => void;
 }
 
@@ -13,15 +16,45 @@ function NoteCard(props: NoteCardProps) {
 	const {
 		note,
 		onCardClick,
+		onLongPress,
 		onTagClick
 	} = props;
+
+	/**
+	 * Ref to track if a long press event was recently triggered.
+	 */
+	const isLongPressedRef = useRef(false);
+
+	/**
+	 * Handles the long press activation.
+	 */
+	const handleLongPress = () => {
+		isLongPressedRef.current = true;
+		onLongPress(note.id);
+	};
+
+	const attrs = useLongPress(handleLongPress, { threshold: 500 });
+
+	/**
+	 * Handles the standard card click,
+	 * preventing action if triggered by a long press.
+	 */
+	const handleClick = () => {
+		if (isLongPressedRef.current) {
+			isLongPressedRef.current = false;
+			return;
+		};
+
+		onCardClick();
+	};
 
 	const dateTimeStr = formatDate(new Date(note.createdAt), { includeTime: true });
 
 	return (
 		<div
+			{...attrs}
 			className={styles.note}
-			onClick={onCardClick}
+			onClick={handleClick}
 		>
 			<NoteText
 				text={note.text}
