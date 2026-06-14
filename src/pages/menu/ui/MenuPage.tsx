34a2782497a7e13f9ae67f8a6687eefde49891e4
Nikolai A. <ilowen@ya.ru>
2026-06-14 22:27:25 +0800

    feat: add PWA installation option to menu

--- src/pages/menu/ui/MenuPage.tsx
+++ src/pages/menu/ui/MenuPage.tsx
@@ -1,7 +1,9 @@
 import styles from './MenuPage.module.css';
 import { useTranslation } from 'react-i18next';
-import packageJson from '../../../../package.json';
+import { MdInstallMobile } from 'react-icons/md';
 import useListItems from '../model/useListItems';
+import packageJson from '../../../../package.json';
+import { usePwaInstall } from '@features/pwa-install';
 import { List } from '@shared/ui';
 
 /**
@@ -10,6 +12,7 @@ import { List } from '@shared/ui';
 function MenuPage() {
 	const { t } = useTranslation();
 	const { habitItems, settingsItems, supportItems } = useListItems();
+	const { status, handleInstall } = usePwaInstall();
 
 	return (
 		<section className={styles.page}>
@@ -29,7 +32,20 @@ function MenuPage() {
 			/>
 
 			<div className={styles.footer}>
-				<small>
+				{status !== 'INSTALLED' && (
+					<List
+						title='PWA'
+						titleStyle={{ color: '#2db78b' }}
+						listStyle={{ border: '2px solid #2db78b' }}
+						items={[{
+							icon: <MdInstallMobile color='#2db78b' />,
+							title: t('welcome.actions.install'),
+							onClick: handleInstall
+						}]}
+					/>
+				)}
+
+				<small className={styles.version}>
 					<span>
 						{t('common.version')}
 					</span>
