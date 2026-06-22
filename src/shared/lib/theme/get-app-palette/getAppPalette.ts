24bb2b11643af84ee58b3b1c0736f9826e1d70ed
Nikolai A. <ilowen@ya.ru>
2026-06-22 22:02:44 +0800

    refactor: update palette colors to use css variables

--- src/shared/lib/theme/get-app-palette/getAppPalette.ts
+++ src/shared/lib/theme/get-app-palette/getAppPalette.ts
@@ -3,18 +3,17 @@ import type { ColorVariants } from '../types';
 const COLORS_COUNT = 21;
 
 /**
- * Generates an array of color variants using OKLCH.
+ * Generates shades referencing App.css variables configured by hue.
  */
 function getAppPalette(): ColorVariants[] {
 	return Array.from({ length: COLORS_COUNT }, (_, i) => {
-		const hue = Math.floor(360 / COLORS_COUNT * i);
+		const hue = Math.floor(360 / COLORS_COUNT * i).toString();
 
 		return {
-			baseColor: `light-dark(oklch(0.8 0.2 ${hue}), oklch(0.6 0.13 ${hue}))`,
-
-			darkenedColor: `light-dark(oklch(0.96 0.02 ${hue}), oklch(0.3 0.01 ${hue}))`,
-
-			softenedColor: `light-dark(oklch(0.7 0.04 ${hue}), oklch(0.58 0.03 ${hue}))`
+			baseColor: 'var(--base-color)',
+			darkenedColor: 'var(--darkened-color)',
+			softenedColor: 'var(--softened-color)',
+			style: { '--hue': hue }
 		};
 	});
 }
--- src/shared/lib/theme/types.ts
+++ src/shared/lib/theme/types.ts
@@ -1,7 +1,10 @@
+import type { CSSProperties } from 'react';
+
 export type ColorScheme = 'light' | 'dark';
 
 export interface ColorVariants {
 	baseColor: string;
 	darkenedColor: string;
 	softenedColor: string;
+	style: CSSProperties & Record<`--${string}`, string>;
 }
