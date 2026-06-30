bb5e52063ef39a7c2134c7f5e9361b92556b455f
Nikolai A. <ilowen@ya.ru>
2026-06-30 15:12:29 +0800

    feat: implement cloudflare pages route for top donors

---
+++ functions/api/donations/top.ts
@@ -0,0 +1,25 @@
+/**
+ * Fetch top 10 donors aggregated by total amount.
+ */
+export const onRequestGet: PagesFunction<Env> = async (context) => {
+	const { env } = context;
+
+	try {
+		const { results } = await env.DB
+			.prepare(`--sql
+				  SELECT user_id,
+					     username,
+					     SUM(amount) as amount
+				    FROM donations
+				GROUP BY user_id
+				ORDER BY amount DESC
+				   LIMIT 10
+			`)
+			.all();
+
+		return Response.json(results);
+	} catch (error) {
+		console.error(error);
+		return new Response('Failed to fetch donations', { status: 500 }); // eslint-disable-line
+	}
+}
