74975a7a0ffdff92a73079cf813e8b92f6ee29ab
Nikolai A. <ilowen@ya.ru>
2026-08-03 19:36:41 +0800

    refactor: update donations schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,8 +1,11 @@
 CREATE TABLE IF NOT EXISTS donations (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id TEXT NOT NULL,
+    client_id TEXT NOT NULL,
+	order_id TEXT NOT NULL UNIQUE,
     username TEXT NOT NULL,
     amount REAL NOT NULL,
     message TEXT,
+	is_anonymous INTEGER NOT NULL DEFAULT 0,
+	status TEXT NOT NULL DEFAULT 'pending',
     created_at INTEGER NOT NULL DEFAULT (strftime('%s', 'now'))
 );
