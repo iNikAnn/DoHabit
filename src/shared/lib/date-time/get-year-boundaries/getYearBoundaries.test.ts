9d75925c1759ad7662122b8c75a5d05ded21bde7
Nikolai A. <ilowen@ya.ru>
2026-09-08 14:39:48 +0800

    test: simplify getYearBoundaries unit tests

--- src/shared/lib/date-time/get-year-boundaries/getYearBoundaries.test.ts
+++ src/shared/lib/date-time/get-year-boundaries/getYearBoundaries.test.ts
@@ -2,21 +2,10 @@ import { describe, expect, test } from 'vitest';
 import { getYearBoundaries } from './getYearBoundaries';
 
 describe('getYearBoundaries', () => {
-	test('returns local midnight through the exclusive start of the next year', () => {
-		const [startTimestamp, endTimestamp] = getYearBoundaries(2025);
-		const start = new Date(startTimestamp);
-		const end = new Date(endTimestamp);
+	test('returns local start of year and start of next year timestamps', () => {
+		const [start, end] = getYearBoundaries(2025);
 
-		expect([start.getFullYear(), start.getMonth(), start.getDate(), start.getHours(), start.getMinutes(), start.getSeconds(), start.getMilliseconds()]).toEqual([2025, 0, 1, 0, 0, 0, 0]);
-		expect([end.getFullYear(), end.getMonth(), end.getDate(), end.getHours(), end.getMinutes(), end.getSeconds(), end.getMilliseconds()]).toEqual([2026, 0, 1, 0, 0, 0, 0]);
-	});
-
-	test('uses the next January 1 after a leap year', () => {
-		const [startTimestamp, endTimestamp] = getYearBoundaries(2024);
-		const start = new Date(startTimestamp);
-		const end = new Date(endTimestamp);
-
-		expect([start.getFullYear(), start.getMonth(), start.getDate()]).toEqual([2024, 0, 1]);
-		expect([end.getFullYear(), end.getMonth(), end.getDate()]).toEqual([2025, 0, 1]);
+		expect(start).toBe(new Date(2025, 0, 1).getTime());
+		expect(end).toBe(new Date(2026, 0, 1).getTime());
 	});
 });
